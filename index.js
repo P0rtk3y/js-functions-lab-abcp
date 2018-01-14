73f9145660da89abf9073c0050c15ad26ca8143a
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-14 06:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-happyHolidays()
 
 function happyHolidaysTo(name){
   return 'Happy holidays, ${name}!';
 }
 
-happyHolidaysTo("lana")
 
 function happyHolidayTo(holiday, name){
   return 'Happy ${holiday}, ${name}!';
 }
 
-happyHolidayTo("Independence Day", "you")
 
 function holidayCountdown(holiday, days){
   return "It's ${days} days until ${holiday}!";
 }
-
-holidayCountdown("Mother's Day", 20)
